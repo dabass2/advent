c15bac794a5a2650e9fb15f74f27f675ec7effd8
Daniel Bass <danielbass123456@gmail.com>
2021-12-06 10:22:48 -0600

    Finish day 6

--- 2021/day6/main.py
+++ 2021/day6/main.py
@@ -1,3 +1,5 @@
+import copy
+
 def read_file(filename):
   with open(filename, 'r') as f:
     rtn = []
@@ -11,50 +13,34 @@ def find_fish(fishes):
     total = total + v
   return total
 
+# This isn't the most memory efficient solution, but it do be runnin fast
 def part1(input, num_days):
-  # fishes = input
-  # fishes = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
   fishes = dict([(8,0),(7,0),(6,0),(5,0),(4,0),(3,0),(2,0),(1,0),(0,0)])
+  delta_fishes = dict([(8,0),(7,0),(6,0),(5,0),(4,0),(3,0),(2,0),(1,0),(0,0)])
   for fish in input:
     fishes[fish] = fishes[fish] + 1
-  print(fishes)
+
   for day in range(1, num_days+1):
-    print("Starting day:", day)
     for k,v in fishes.items():
-      print(k,v)
-      if v > 0 and k != 0:
-        fishes[k] = fishes[k] - v
-        fishes[k+1] = fishes[k+1] + v
+      if v > 0 and k > 0: # for every value, store the days changes in another dict
+        delta_fishes[k-1] = delta_fishes[k-1] + v
       elif k == 0 and v > 0:
-        fishes[0] = 0
-        fishes[8] = fishes[8] + v
-    print("After day:", day, fishes)
+        delta_fishes[6] = delta_fishes[6] + v
+        delta_fishes[8] = delta_fishes[8] + v
+    fishes = copy.deepcopy(delta_fishes)  # then at the end of the day, apply the changes
+    delta_fishes = dict([(8,0),(7,0),(6,0),(5,0),(4,0),(3,0),(2,0),(1,0),(0,0)])
   print("Number of fishes:", find_fish(fishes))
 
-def part2(input, num_days):
-  fishes = input
-  for day in range(1, num_days+1):
-    # print("Starting day:", day)
-    for i in range(len(fishes)):
-      fish = fishes[i]
-      if fish == 0:
-        fishes[i] = 6
-        fishes.append(8)
-      else:
-        fishes[i] = fish - 1
-    # print("After day:", day, fishes)
-  print("Number of fishes:", len(fishes))
-
 def main():
-  input = read_file('test.file')
+  input = read_file('input.file')
   print("-----------------")
   print("Running part 1...")
-  days1 = 2
+  days1 = 80
   part1(input, days1)
   print("-----------------")
-  # print("Running part 2...")
-  # days2 = 256
-  # part2(input, days2)
-  # print("-----------------")
+  print("Running part 2...")
+  days2 = 256
+  part1(input, days2)
+  print("-----------------")
 
 main()
